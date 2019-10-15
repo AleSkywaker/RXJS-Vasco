705eb1e98ad05cff37f0a416d92ff5f2b915fe45
Alex Colombo <uricaine@hotmail.com>
2019-10-15 22:00:03 +0200

    new component generated

---
+++ src/app/browser-event-experiment/browser-event-experiment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'browser-event-experiment',
+  templateUrl: './browser-event-experiment.component.html',
+  styleUrls: ['./browser-event-experiment.component.css']
+})
+export class BrowserEventExperimentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
